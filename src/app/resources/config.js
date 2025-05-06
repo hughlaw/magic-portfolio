c42f839ad64f5846533aecf14ad1f72208f086b8
Hugh Law <hughlaw1+github@gmail.com>
2025-05-06 23:18:06 +0100

    chore: update readme, minor style tweaks

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -40,8 +40,8 @@ const font = {
 const style = {
   theme: 'dark', // theme is not necessary when using ThemeProvider (default)
   neutral: 'gray', // sand | gray | slate
-  brand: 'cyan', // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-  accent: 'red', // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  brand: 'magenta', // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  accent: 'blue', // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
   solid: 'contrast', // color | contrast
   solidStyle: 'flat', // flat | plastic
   border: 'playful', // rounded | playful | conservative
@@ -83,10 +83,10 @@ const effects = {
   },
   lines: {
     display: false,
-    opacity: 100,
+    opacity: 40,
     color: 'neutral-alpha-weak',
     size: '16',
-    thickness: 1,
+    thickness: 2,
     angle: 45,
   },
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,34 @@
-"use client";
+'use client';
 
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-import { Fade, Flex, Line, ToggleButton } from "@/once-ui/components";
-import styles from "@/components/Header.module.scss";
+import { Fade, Flex, Line, ToggleButton } from '@/once-ui/components';
+import styles from '@/components/Header.module.scss';
 
-import { routes, display } from "@/app/resources";
-import { person, about, blog, work, gallery } from "@/app/resources/content";
-import { ThemeToggle } from "./ThemeToggle";
+import { routes, display } from '@/app/resources';
+import { person, about, blog, work, gallery } from '@/app/resources/content';
+import { ThemeToggle } from './ThemeToggle';
 
 type TimeDisplayProps = {
   timeZone: string;
   locale?: string; // Optionally allow locale, defaulting to 'en-GB'
 };
 
-const TimeDisplay: React.FC<TimeDisplayProps> = ({ timeZone, locale = "en-GB" }) => {
-  const [currentTime, setCurrentTime] = useState("");
+const TimeDisplay: React.FC<TimeDisplayProps> = ({
+  timeZone,
+  locale = 'en-GB',
+}) => {
+  const [currentTime, setCurrentTime] = useState('');
 
   useEffect(() => {
     const updateTime = () => {
       const now = new Date();
       const options: Intl.DateTimeFormatOptions = {
         timeZone,
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
         hour12: false,
       };
       const timeString = new Intl.DateTimeFormat(locale, options).format(now);
@@ -44,12 +47,20 @@ const TimeDisplay: React.FC<TimeDisplayProps> = ({ timeZone, locale = "en-GB" })
 export default TimeDisplay;
 
 export const Header = () => {
-  const pathname = usePathname() ?? "";
+  const pathname = usePathname() ?? '';
 
   return (
     <>
       <Fade hide="s" fillWidth position="fixed" height="80" zIndex={9} />
-      <Fade show="s" fillWidth position="fixed" bottom="0" to="top" height="80" zIndex={9} />
+      <Fade
+        show="s"
+        fillWidth
+        position="fixed"
+        bottom="0"
+        to="top"
+        height="80"
+        zIndex={9}
+      />
       <Flex
         fitHeight
         position="unset"
@@ -61,7 +72,12 @@ export const Header = () => {
         horizontal="center"
         data-border="rounded"
       >
-        <Flex paddingLeft="12" fillWidth vertical="center" textVariant="body-default-s">
+        <Flex
+          paddingLeft="12"
+          fillWidth
+          vertical="center"
+          textVariant="body-default-s"
+        >
           {display.location && <Flex hide="s">{person.location}</Flex>}
         </Flex>
         <Flex fillWidth horizontal="center">
@@ -75,75 +91,79 @@ export const Header = () => {
             zIndex={1}
           >
             <Flex gap="4" vertical="center" textVariant="body-default-s">
-              {routes["/"] && (
-                <ToggleButton prefixIcon="home" href="/" selected={pathname === "/"} />
+              {routes['/'] && (
+                <ToggleButton
+                  prefixIcon="home"
+                  href="/"
+                  selected={pathname === '/'}
+                />
               )}
               <Line background="neutral-alpha-medium" vert maxHeight="24" />
-              {routes["/about"] && (
+              {routes['/about'] && (
                 <>
                   <ToggleButton
                     className="s-flex-hide"
                     prefixIcon="person"
                     href="/about"
                     label={about.label}
-                    selected={pathname === "/about"}
+                    selected={pathname === '/about'}
                   />
                   <ToggleButton
                     className="s-flex-show"
                     prefixIcon="person"
                     href="/about"
-                    selected={pathname === "/about"}
+                    selected={pathname === '/about'}
                   />
                 </>
               )}
-              {routes["/work"] && (
+              {routes['/work'] && (
                 <>
                   <ToggleButton
                     className="s-flex-hide"
                     prefixIcon="grid"
                     href="/work"
                     label={work.label}
-                    selected={pathname.startsWith("/work")}
+                    selected={pathname.startsWith('/work')}
                   />
                   <ToggleButton
                     className="s-flex-show"
                     prefixIcon="grid"
                     href="/work"
-                    selected={pathname.startsWith("/work")}
+                    selected={pathname.startsWith('/work')}
                   />
                 </>
               )}
-              {routes["/blog"] && (
+              {routes['/blog'] && (
                 <>
                   <ToggleButton
                     className="s-flex-hide"
                     prefixIcon="book"
                     href="/blog"
                     label={blog.label}
-                    selected={pathname.startsWith("/blog")}
+                    selected={pathname.startsWith('/blog')}
                   />
                   <ToggleButton
                     className="s-flex-show"
                     prefixIcon="book"
                     href="/blog"
-                    selected={pathname.startsWith("/blog")}
+                    selected={pathname.startsWith('/blog')}
                   />
                 </>
               )}
-              {routes["/gallery"] && (
+              {routes['/gallery'] && (
                 <>
                   <ToggleButton
                     className="s-flex-hide"
                     prefixIcon="gallery"
                     href="/gallery"
                     label={gallery.label}
-                    selected={pathname.startsWith("/gallery")}
+                    selected={pathname.startsWith('/gallery')}
                   />
                   <ToggleButton
                     className="s-flex-show"
                     prefixIcon="gallery"
                     href="/gallery"
-                    selected={pathname.startsWith("/gallery")}
+                    selected={pathname.startsWith('/gallery')}
                   />
                 </>
               )}
@@ -164,7 +184,9 @@ export const Header = () => {
             textVariant="body-default-s"
             gap="20"
           >
-            <Flex hide="s">{display.time && <TimeDisplay timeZone={person.location} />}</Flex>
+            <Flex hide="s">
+              {display.time && <TimeDisplay timeZone={person.location} />}
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
