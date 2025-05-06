9bccde5c93b3e6cd471601673f763431541a987d
Hugh Law <hughlaw1+github@gmail.com>
2025-05-06 23:02:10 +0100

    Update with initial data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React from 'react';
 
-import { Heading, Flex, Text, Button, Avatar, RevealFx, Column, Badge, Row } from "@/once-ui/components";
-import { Projects } from "@/components/work/Projects";
+import {
+  Heading,
+  Flex,
+  Text,
+  Button,
+  Avatar,
+  RevealFx,
+  Column,
+  Badge,
+  Row,
+} from '@/once-ui/components';
+import { Projects } from '@/components/work/Projects';
 
-import { baseURL, routes } from "@/app/resources";
-import { home, about, person, newsletter } from "@/app/resources/content";
-import { Mailchimp } from "@/components";
-import { Posts } from "@/components/blog/Posts";
-import { Meta, Schema } from "@/once-ui/modules";
+import { baseURL, routes } from '@/app/resources';
+import { home, about, person, newsletter } from '@/app/resources/content';
+import { Mailchimp } from '@/components';
+import { Posts } from '@/components/blog/Posts';
+import { Meta, Schema } from '@/once-ui/modules';
 
 export async function generateMetadata() {
   return Meta.generate({
@@ -36,25 +46,58 @@ export default function Home() {
       />
       <Column fillWidth paddingY="24" gap="m">
         <Column maxWidth="s">
-          {home.featured && (
-          <RevealFx fillWidth horizontal="start" paddingTop="16" paddingBottom="32" paddingLeft="12">
-            <Badge background="brand-alpha-weak" paddingX="12" paddingY="4" onBackground="neutral-strong" textVariant="label-default-s" arrow={false}
-              href={home.featured.href}>
-              <Row paddingY="2">{home.featured.title}</Row>
-            </Badge>
-          </RevealFx>
+          {home.featured && home.featured.display && (
+            <RevealFx
+              fillWidth
+              horizontal="start"
+              paddingTop="16"
+              paddingBottom="32"
+              paddingLeft="12"
+            >
+              <Badge
+                background="brand-alpha-weak"
+                paddingX="12"
+                paddingY="4"
+                onBackground="neutral-strong"
+                textVariant="label-default-s"
+                arrow={false}
+                href={home.featured.href}
+              >
+                <Row paddingY="2">{home.featured.title}</Row>
+              </Badge>
+            </RevealFx>
           )}
-          <RevealFx translateY="4" fillWidth horizontal="start" paddingBottom="16">
+          <RevealFx
+            translateY="4"
+            fillWidth
+            horizontal="start"
+            paddingBottom="16"
+          >
             <Heading wrap="balance" variant="display-strong-l">
               {home.headline}
             </Heading>
           </RevealFx>
-          <RevealFx translateY="8" delay={0.2} fillWidth horizontal="start" paddingBottom="32">
-            <Text wrap="balance" onBackground="neutral-weak" variant="heading-default-xl">
+          <RevealFx
+            translateY="8"
+            delay={0.2}
+            fillWidth
+            horizontal="start"
+            paddingBottom="32"
+          >
+            <Text
+              wrap="balance"
+              onBackground="neutral-weak"
+              variant="heading-default-xl"
+            >
               {home.subline}
             </Text>
           </RevealFx>
-          <RevealFx paddingTop="12" delay={0.4} horizontal="start" paddingLeft="12">
+          <RevealFx
+            paddingTop="12"
+            delay={0.4}
+            horizontal="start"
+            paddingLeft="12"
+          >
             <Button
               id="about"
               data-border="rounded"
@@ -66,7 +109,7 @@ export default function Home() {
               <Flex gap="8" vertical="center">
                 {about.avatar.display && (
                   <Avatar
-                    style={{ marginLeft: "-0.75rem", marginRight: "0.25rem" }}
+                    style={{ marginLeft: '-0.75rem', marginRight: '0.25rem' }}
                     src={person.avatar}
                     size="m"
                   />
@@ -77,10 +120,10 @@ export default function Home() {
           </RevealFx>
         </Column>
       </Column>
-      <RevealFx translateY="16" delay={0.6}>
+      {/* <RevealFx translateY="16" delay={0.6}>
         <Projects range={[1, 1]} />
-      </RevealFx>
-      {routes["/blog"] && (
+      </RevealFx> */}
+      {/* {routes['/blog'] && (
         <Flex fillWidth gap="24" mobileDirection="column">
           <Flex flex={1} paddingLeft="l" paddingTop="24">
             <Heading as="h2" variant="display-strong-xs" wrap="balance">
@@ -91,8 +134,8 @@ export default function Home() {
             <Posts range={[1, 2]} columns="2" />
           </Flex>
         </Flex>
-      )}
-      <Projects range={[2]} />
+      )} */}
+      <Projects />
       {newsletter.display && <Mailchimp newsletter={newsletter} />}
     </Column>
   );
--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,25 +1,25 @@
-import { Logo } from "@/once-ui/components";
+import { Flex, Logo } from '@/once-ui/components';
 
 const person = {
-  firstName: "Selene",
-  lastName: "Yu",
+  firstName: 'Hugh',
+  lastName: 'Law',
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
-  role: "Design Engineer",
-  avatar: "/images/avatar.jpg",
-  email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  role: 'Senior Frontend/UX Engineer',
+  avatar: '/images/avatar.png',
+  email: 'hello@hughlaw.dev',
+  location: 'Europe/Dublin', // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: [], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter = {
-  display: true,
+  display: false,
   title: <>Subscribe to {person.firstName}'s Newsletter</>,
   description: (
     <>
-      I occasionally write about design, technology, and share thoughts on the intersection of
-      creativity and engineering.
+      I occasionally write about design, technology, and share thoughts on the
+      intersection of creativity and engineering.
     </>
   ),
 };
@@ -28,50 +28,61 @@ const social = [
   // Links are automatically displayed.
   // Import new icons in /once-ui/icons.ts
   {
-    name: "GitHub",
-    icon: "github",
-    link: "https://github.com/once-ui-system/nextjs-starter",
+    name: 'GitHub',
+    icon: 'github',
+    link: 'https://github.com/hughlaw',
   },
   {
-    name: "LinkedIn",
-    icon: "linkedin",
-    link: "https://www.linkedin.com/company/once-ui/",
+    name: 'LinkedIn',
+    icon: 'linkedin',
+    link: 'https://www.linkedin.com/in/hughlaw/',
   },
   {
-    name: "X",
-    icon: "x",
-    link: "",
+    name: 'X',
+    icon: 'x',
+    link: '',
   },
   {
-    name: "Email",
-    icon: "email",
+    name: 'Email',
+    icon: 'email',
     link: `mailto:${person.email}`,
   },
 ];
 
 const home = {
-  path: "/",
-  image: "/images/og/home.jpg",
-  label: "Home",
+  path: '/',
+  image: '/images/og/home.jpg',
+  label: 'Home',
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>Building bridges between design and code</>,
+  // headline: <>Designing, developing and delivering digital delights</>,
+  headline: <>Turning complex ideas into simple clicks</>,
   featured: {
-    display: true,
-    title: <>Recent project: <strong className="ml-4">Once UI</strong></>,
-    href: "/work/building-once-ui-a-customizable-design-system",
+    display: false,
+    title: (
+      <>
+        Recent project: <strong className="ml-4">Once UI</strong>
+      </>
+    ),
+    href: '/work/building-once-ui-a-customizable-design-system',
   },
   subline: (
-    <>
-      I'm Selene, a design engineer at <Logo icon={false} style={{ display: "inline-flex", top: "0.25em", marginLeft: "-0.25em" }}/>, where I craft intuitive
-      <br /> user experiences. After hours, I build my own projects.
-    </>
+    <Flex direction="column" gap="12">
+      <p>
+        I'm Hugh, a Senior Frontend/UX Engineer at Hedgey Finance,
+        <br /> bridging the gap between design and development.
+      </p>
+      <p>
+        I specialize in designing and building beautiful, accessible, and
+        performant web applications.
+      </p>
+    </Flex>
   ),
 };
 
 const about = {
-  path: "/about",
-  label: "About",
+  path: '/about',
+  label: 'About',
   title: `About – ${person.name}`,
   description: `Meet ${person.name}, ${person.role} from ${person.location}`,
   tableOfContent: {
@@ -82,60 +93,171 @@ const about = {
     display: true,
   },
   calendar: {
-    display: true,
-    link: "https://cal.com",
+    display: false,
+    link: 'https://cal.com',
   },
   intro: {
     display: true,
-    title: "Introduction",
+    title: 'Introduction',
     description: (
       <>
-        Selene is a Jakarta-based design engineer with a passion for transforming complex challenges
-        into simple, elegant design solutions. Her work spans digital interfaces, interactive
-        experiences, and the convergence of design and technology.
+        Hugh is a Ireland-based developer with a unique skill set the blends UX
+        design with frontend development. With 20+ years experience designing
+        and building web experiences from concept to production, he is able to
+        uniquely navigate both design and development challenges.
       </>
     ),
   },
   work: {
     display: true, // set to false to hide this section
-    title: "Work Experience",
+    title: 'Work Experience',
     experiences: [
       {
-        company: "FLY",
-        timeframe: "2022 - Present",
-        role: "Senior Design Engineer",
+        company: 'Hedgey Finance',
+        timeframe: 'Dec 2021 – Present',
+        role: 'Senior Frontend/UX Engineer',
         achievements: [
           <>
-            Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user
-            engagement and 30% faster load times.
+            Designed and developed component libraries using Storybook,
+            streamlining the UI development process, allowing us to deliver
+            performant, maintainable, and scalable UIs.
           </>,
           <>
-            Spearheaded the integration of AI tools into design workflows, enabling designers to
-            iterate 50% faster.
+            Implemented{' '}
+            <a href="https://app.hedgey.finance" target="_blank">
+              app.hedgey.finance
+            </a>{' '}
+            which has facilitated over{' '}
+            <strong>$1B in transaction volume</strong> and secures over{' '}
+            <strong>$250M in Total Value Locked (TVL)</strong> through its
+            interfaces and smart contracts.
+          </>,
+          <>
+            Developed airdrop claim portals in NextJS to give clients custom UIs
+            to integrate with Hedgey's claim APIs.
+          </>,
+          <>
+            Developed intuitive UX flows tailored for Web3, simplifying complex
+            blockchain concepts for users.
+          </>,
+          <>
+            Collaborated with the senior team to create dashboards and UI flows
+            for vesting plans and token unlock campaigns.
           </>,
         ],
         images: [
-          // optional: leave the array empty if you don't want to display images
           {
-            src: "/images/projects/project-01/cover-01.jpg",
-            alt: "Once UI Project",
+            src: '/images/projects/hedgey/hedgey-01.jpeg',
+            alt: 'Hedgey Finance Project',
+            width: 16,
+            height: 9,
+          },
+          {
+            src: '/images/projects/hedgey/hedgey-02.jpeg',
+            alt: 'Hedgey Finance Project',
             width: 16,
             height: 9,
           },
         ],
       },
       {
-        company: "Creativ3",
-        timeframe: "2018 - 2022",
-        role: "Lead Designer",
+        company: 'Better Examinations Online',
+        timeframe: 'Mar 2020 – Dec 2021',
+        role: 'UX Lead / Frontend Developer',
         achievements: [
           <>
-            Developed a design system that unified the brand across multiple platforms, improving
-            design consistency by 40%.
+            Primary advocate for user-based decisions in product roadmap and UX
+            activities.
           </>,
           <>
-            Led a cross-functional team to launch a new product line, contributing to a 15% increase
-            in overall company revenue.
+            Developed and maintained the BE UI component library and style
+            guide.
+          </>,
+          <>
+            Moved Better Examinations Online from proof of concept to production
+            app with several high-profile clients.
+          </>,
+        ],
+        images: [
+          {
+            src: '/images/projects/better-examinations/better-examinations-01.jpeg',
+            alt: 'Better Examinations Project',
+            width: 16,
+            height: 9,
+          },
+          {
+            src: '/images/projects/better-examinations/better-examinations-02.jpeg',
+            alt: 'Better Examinations Project',
+            width: 16,
+            height: 9,
+          },
+        ],
+      },
+      {
+        company: 'Terminalfour',
+        timeframe: 'May 2014 – Mar 2020',
+        role: 'UX Lead / Frontend Developer',
+        achievements: [
+          <>
+            Reimagined, designed, and drove the implementation of new product UI
+            from v7 to v8 (the biggest in company history).
+          </>,
+          <>
+            Created a product style and implementation guide for a consistent
+            look and feel across the product.
+          </>,
+          <>
+            Developed a frontend component library to ensure UI elements are
+            updated and maintained easily.
+          </>,
+        ],
+        images: [
+          {
+            src: '/images/projects/terminalfour/terminalfour-01.jpeg',
+            alt: 'Terminalfour Project',
+            width: 16,
+            height: 9,
+          },
+        ],
+      },
+      {
+        company: 'Terminalfour',
+        timeframe: 'May 2011 – May 2014',
+        role: 'Web Developer / Designer',
+        achievements: [
+          <>
+            Worked on large-scale client projects for universities and
+            organizations in Europe, the US, and Australia, including the OECD,
+            RMIT University, Liverpool Hope University, UCD Dublin and many
+            more.
+          </>,
+          <>
+            Implemented client sites using the company CMS and collaborated
+            directly with clients and internal teams.
+          </>,
+        ],
+        images: [],
+      },
+      {
+        company: 'Singularity',
+        timeframe: 'Jun 2009 – Sep 2009',
+        role: 'HCI Designer',
+        achievements: [
+          <>
+            Developed a new user interface for Singularity's LiveAgility BPM
+            Software, an online SaaS version of their BPM software.
+          </>,
+        ],
+        images: [],
+      },
+      {
+        company: 'Marble Multimedia',
+        timeframe: 'Jan 2002 – May 2011',
+        role: 'Freelance Web Developer / Graphic Designer',
+        achievements: [
+          <>
+            Worked as a freelance web developer and graphic designer for a
+            variety of clients and projects.
           </>,
         ],
         images: [],
@@ -144,70 +266,73 @@ const about = {
   },
   studies: {
     display: true, // set to false to hide this section
-    title: "Studies",
+    title: 'Education',
     institutions: [
       {
-        name: "University of Jakarta",
-        description: <>Studied software engineering.</>,
+        name: 'University of Ulster',
+        description: <>MSc Computing & Creative Technology - Distinction</>,
       },
       {
-        name: "Build the Future",
-        description: <>Studied online marketing and personal branding.</>,
+        name: 'University of Ulster',
+        description: (
+          <>BSc Hon Multimedia Computing & Design - 1st Class Honours</>
+        ),
       },
     ],
   },
   technical: {
     display: true, // set to false to hide this section
-    title: "Technical skills",
+    title: 'Technical skills',
     skills: [
       {
-        title: "Figma",
-        description: <>Able to prototype in Figma with Once UI with unnatural speed.</>,
+        title: 'Figma',
+        description: <>Ability to design and prototype in Figma.</>,
         // optional: leave the array empty if you don't want to display images
-        images: [
-          {
-            src: "/images/projects/project-01/cover-02.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-          {
-            src: "/images/projects/project-01/cover-03.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-        ],
+        images: [],
       },
       {
-        title: "Next.js",
-        description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
+        title: 'Svelte / Sveltekit',
+        description: (
+          <>Good understanding of the Svelte framework and ecosystem.</>
+        ),
         // optional: leave the array empty if you don't want to display images
-        images: [
-          {
-            src: "/images/projects/project-01/cover-04.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-        ],
+        images: [],
+      },
+      {
+        title: 'React, Next.js',
+        description: (
+          <>Knowledge and experience developing with React and Next.js</>
+        ),
+        // optional: leave the array empty if you don't want to display images
+        images: [],
+      },
+      {
+        title: 'Cypress, Jest, Jasmine, Playwright',
+        description: (
+          <>
+            Knowledge and experience with testing frameworks for unit,
+            integration and e2e testing.
+          </>
+        ),
+        // optional: leave the array empty if you don't want to display images
+        images: [],
       },
     ],
   },
 };
 
 const blog = {
-  path: "/blog",
-  label: "Blog",
-  title: "Writing about design and tech...",
+  path: '/blog',
+  label: 'Blog',
+  title: 'Writing about design and tech...',
   description: `Read what ${person.name} has been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
 };
 
 const work = {
-  path: "/work",
-  label: "Work",
+  path: '/work',
+  label: 'Work',
   title: `Projects – ${person.name}`,
   description: `Design and dev projects by ${person.name}`,
   // Create new project pages by adding a new .mdx file to app/blog/posts
@@ -215,52 +340,52 @@ const work = {
 };
 
 const gallery = {
-  path: "/gallery",
-  label: "Gallery",
+  path: '/gallery',
+  label: 'Gallery',
   title: `Photo gallery – ${person.name}`,
   description: `A photo collection by ${person.name}`,
   // Images by https://lorant.one
   // These are placeholder images, replace with your own
   images: [
     {
-      src: "/images/gallery/horizontal-1.jpg",
-      alt: "image",
-      orientation: "horizontal",
+      src: '/images/gallery/horizontal-1.jpg',
+      alt: 'image',
+      orientation: 'horizontal',
     },
     {
-      src: "/images/gallery/horizontal-2.jpg",
-      alt: "image",
-      orientation: "horizontal",
+      src: '/images/gallery/horizontal-2.jpg',
+      alt: 'image',
+      orientation: 'horizontal',
     },
     {
-      src: "/images/gallery/horizontal-3.jpg",
-      alt: "image",
-      orientation: "horizontal",
+      src: '/images/gallery/horizontal-3.jpg',
+      alt: 'image',
+      orientation: 'horizontal',
     },
     {
-      src: "/images/gallery/horizontal-4.jpg",
-      alt: "image",
-      orientation: "horizontal",
+      src: '/images/gallery/horizontal-4.jpg',
+      alt: 'image',
+      orientation: 'horizontal',
     },
     {
-      src: "/images/gallery/vertical-1.jpg",
-      alt: "image",
-      orientation: "vertical",
+      src: '/images/gallery/vertical-1.jpg',
+      alt: 'image',
+      orientation: 'vertical',
     },
     {
-      src: "/images/gallery/vertical-2.jpg",
-      alt: "image",
-      orientation: "vertical",
+      src: '/images/gallery/vertical-2.jpg',
+      alt: 'image',
+      orientation: 'vertical',
     },
     {
-      src: "/images/gallery/vertical-3.jpg",
-      alt: "image",
-      orientation: "vertical",
+      src: '/images/gallery/vertical-3.jpg',
+      alt: 'image',
+      orientation: 'vertical',
     },
     {
-      src: "/images/gallery/vertical-4.jpg",
-      alt: "image",
-      orientation: "vertical",
+      src: '/images/gallery/vertical-4.jpg',
+      alt: 'image',
+      orientation: 'vertical',
     },
   ],
 };
